40dfcf24f4c99a34a69aa8062b097b6c71eaf49d
guojinliang <496168739@qq.com>
2021-07-28 17:09:28 +0800

    add bubbleSort

---
+++ sort/bubbleSort.js
@@ -0,0 +1,17 @@
+Array.prototype.bubbleSort = function () {
+  for (let i = 0; i < this.length - 1; i++) {
+    for (let j = 0; j < this.length - i - 1; j++) {
+      if (this[j] > this[j + 1]) {
+        const temp = this[j];
+        this[j] = this[j + 1];
+        this[j + 1] = temp;
+      }
+    }
+  }
+};
+
+const arr = [5, 4, 3, 2, 1];
+
+arr.bubbleSort();
+
+console.log(arr); // [1,2,3,4,5]  o(n^2)
